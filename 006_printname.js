47a1675413feb46bcda0597b5b07b76ea186027f
ayadij <ayadijkwel@yahoo.com>
2018-03-12 12:13:48 -0600

    tsc printname files and added comments

--- 006_printname.js
+++ 006_printname.js
@@ -5,4 +5,10 @@ function printName(f, l) {
     console.log(greeting + f + " " + l);
 }
 printName("Aya", "Dijkwel");
+// cannot redefine a let statement
+// let defines the scope inside function and restricts it
+// var is considered to be a global variable bc it has global type properties
+// let greeting : string = "Hi there "
+// vs
+// var greeting : string = "Hi there, ";
 //# sourceMappingURL=006_printname.js.map
--- 006_printname.ts
+++ 006_printname.ts
@@ -5,5 +5,12 @@ function printName(f, l) {
   var greeting : string = "Hey there, ";
   console.log(greeting + f + " " + l);
 }
-
 printName("Aya", "Dijkwel");
+
+
+// cannot redefine a let statement
+// let defines the scope inside function and restricts it
+// var is considered to be a global variable bc it has global type properties
+      // let greeting : string = "Hi there "
+                  // vs
+      // var greeting : string = "Hi there, ";
