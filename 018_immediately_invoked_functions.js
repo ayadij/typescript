3145ce7644573df85793d4567190f727eaa555bd
ayadij <ayadijkwel@yahoo.com>
2018-03-13 13:56:34 -0600

    immediately invoked functions

---
+++ 018_immediately_invoked_functions.js
@@ -0,0 +1,10 @@
+var names = ['Jordan', 'Tiffany', 'Kristine'];
+var counter = 0;
+(function () {
+    for (var name_1 in names) {
+        counter++;
+    }
+})();
+console.log(counter);
+// functions that are ran whent he interpreter or parser reaches that line of code
+//# sourceMappingURL=018_immediately_invoked_functions.js.map
---
+++ 018_immediately_invoked_functions.ts
@@ -0,0 +1,14 @@
+
+var names : string[] = ['Jordan', 'Tiffany', 'Kristine'];
+var counter : number = 0;
+
+(function() {
+  for (let name in names) {
+    counter++;
+  }
+})();
+
+console.log(counter);
+
+
+// functions that are ran whent he interpreter or parser reaches that line of code
