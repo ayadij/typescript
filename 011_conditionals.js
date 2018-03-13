9d5bb151e31e22ec5df1b3eb9153195dc4b6a54f
ayadij <ayadijkwel@yahoo.com>
2018-03-13 10:20:47 -0600

    conditionals

---
+++ 011_conditionals.js
@@ -0,0 +1,11 @@
+var favorite_icecream = 'turkey hill salted caramel in vanilla bean';
+if (favorite_icecream == 'ben and jerrys half baked') {
+    console.log('yes, ben and jerrys half baked is my favorite icecream');
+}
+else if (favorite_icecream == 'turkey hill salted caramel in vanilla bean') {
+    console.log('yes, turkey hill salted caramel in vanilla bean is my favorite icecream');
+}
+else {
+    console.log('sorry, you must not know my favorite icecream');
+}
+//# sourceMappingURL=011_conditionals.js.map
--- 011_conditionals.ts
+++ 011_conditionals.ts
@@ -1,9 +1,9 @@
-let password : string = 'zxcvzxcv';
+let favorite_icecream : string = 'turkey hill salted caramel in vanilla bean';
 
-if(password == 'asdfasdf') {
-  console.log('Yes, asdfasdf is the password');
-} else if(password == 'zxcvzxcv') {
-  console.log('Yes, zxcvzxcv is the password');
+if(favorite_icecream == 'ben and jerrys half baked') {
+  console.log('yes, ben and jerrys half baked is my favorite icecream');
+} else if(favorite_icecream == 'turkey hill salted caramel in vanilla bean') {
+  console.log('yes, turkey hill salted caramel in vanilla bean is my favorite icecream');
 } else {
-  console.log('Sorry, permission denied');
+  console.log('sorry, you must not know my favorite icecream');
 }
